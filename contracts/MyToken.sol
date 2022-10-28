e8fa5fbdba67dd08202bfae18f7367ed5766188d
Joaquin <95145404+joacoabraldes@users.noreply.github.com>
2022-10-28 11:53:40 -0300

    ...

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.9;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract MyToken is ERC20 {
-    constructor() ERC20("Whey Protein Token", "WPT"){
+    constructor() ERC20("Whey Protein Token", "WPT") // Asignando nombre al token {
         _mint(msg.sender, 100000);
     }
 }
