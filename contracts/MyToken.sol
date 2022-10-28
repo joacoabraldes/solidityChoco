d683c44b1c77907ecc0f3fef2ecba32f5ee3753a
Joaquin <95145404+joacoabraldes@users.noreply.github.com>
2022-10-28 12:19:18 -0300

    ...

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -1,10 +1,10 @@
-// SPDX-License-Identifier: <SPDX-License>
+// SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract MyToken is ERC20 {
-    constructor() ERC20("Whey Protein Token", "WPT") // Asignando nombre al token {
+    constructor() ERC20("Whey Protein Token", "WPT") { 
         _mint(msg.sender, 100);
     }
 }
